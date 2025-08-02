18b92a7d89e91356a6c7983312b07dbe89127b29
Mustafa <imos8afa@gmail.com>
2025-08-02 15:12:26 +0300

    Applying the file handling on the P1 project

--- Projects/P1.py
+++ Projects/P1.py
@@ -1,6 +1,3 @@
-myTasks = []
-endTasks = []
-
 def TaskManager():
     while True:
         print("__ TASK--MANAGER __")
@@ -26,42 +23,29 @@ def TaskManager():
                 print("Good bye")
                 break
             else:
+                
                 print("Invalid input")
         except:
             print("Please enter a valid number")
 
 def AddTask():
     while True:
-        myTasks.append((input("Enter the task: ").strip()).title())
+        
+        with open(r"D:\Python Basics\Projects\Tasks.txt", "r") as file:
+            tasksList = [line.strip() for line in file.readlines()]
+       
+        new_task = input("Enter the task: ").strip().title()
+        tasksList.append(new_task)
+       
+        with open(r"D:\Python Basics\Projects\Tasks.txt", "w") as file:
+            for task in tasksList:
+                file.write(task + "\n")
+
         print("Task has been added successfully")
         print("[1] Add another task...")
         print("[2] Back...")
-        i = int(input("Enter the number of function: ").strip())
-        if i == 1:
-            continue
-        elif i == 2:
-            break
-        else:
-            print("Enter a valid input")    
 
-def ViewAllTasks():
-    if len(myTasks) >=1:
-        for task in myTasks:
-            print(task)
-    else:
-        print("There is no tasks")
-    input("Press enter to get back...")
-
-def DeleteTask():
-    while True:
         try:
-            if len(myTasks) >= 1:
-                myTasks.remove((input("Enter task name : ").strip()).title())
-                print("Task has been Deleted successfully")
-            else:
-                print("There is no tasks")
-            print("[1] Delete another task...")
-            print("[2] Back...")
             i = int(input("Enter the number of function: ").strip())
             if i == 1:
                 continue
@@ -69,35 +53,110 @@ def DeleteTask():
                 break
             else:
                 print("Enter a valid input")
+        except:
+            print("Invalid number, going back...")
+            break
+
+def ViewAllTasks():
+    if len(open(r"D:\Python Basics\Projects\Tasks.txt","r",encoding='utf-8').read()) >=1 :
+        myTasks = open(r"D:\Python Basics\Projects\Tasks.txt","r")
+        tasks = myTasks.readlines()
+        for task in tasks:
+            print(task)
+
+    else:
+        print("There is no tasks")
+    input("Press enter to get back...")
+    myTasks.close()
+
+def DeleteTask():
+    while True:
+        try:
+            if len(open(r"D:\Python Basics\Projects\Tasks.txt","r",encoding='utf-8').read()) >=1 :
+                myTasks = open(r"D:\Python Basics\Projects\Tasks.txt","r")     
+                content = [line.strip() for line in myTasks.readlines()]
+                content.remove((input("Enter the task : ").strip()).title())     
+                myTasks.close()
+
+                myTasks = open(r"D:\Python Basics\Projects\Tasks.txt","w")
+                for task in content:
+                    myTasks.write(task+"\n")
+                myTasks.close()  
+           
+                print("Task has been Deleted successfully")
+
+            else:
+                print("There is no tasks")
+            print("[1] Delete another task...")
+            print("[2] Back...")
+
+            i = int(input("Enter the number of function: ").strip())
+
+            if i == 1:
+                continue
+
+            elif i == 2:
+                break
+
+            else:
+                print("Enter a valid input")
+
         except:
             print("Enter a Valid task name")          
 
 def EndTask():
     while True:
         try:
-            if len(myTasks) >= 1:
-                endTask = input("Enter the task : ").strip().title()
-                endTasks.append(endTask)
-                myTasks.remove(endTask)
+            if len(open(r"D:\Python Basics\Tasks.txt","r",encoding='utf-8').read()) >=1:
+                endTasks = open(r"D:\Python Basics\Projects\EndTasks.txt","r")
+                endTaskList = [line.strip() for line in endTasks.readlines()]
+                endTasks.close()
+                endTask = (input("Enter the task : ").strip()).title()
+                endTaskList.append(endTask)
+
+                endTasks = open(r"D:\Python Basics\Projects\EndTasks.txt","w")
+                for endTask in endTaskList :
+                    endTasks.write(endTask +"\n")
+                endTasks.close()
+
+                myTasks = open(r"D:\Python Basics\Tasks.txt","r")     
+                content = [line.strip() for line in myTasks.readlines()]
+                content.remove(endTask)     
+                myTasks.close()
+
+                myTasks = open(r"D:\Python Basics\Tasks.txt","w")
+                for task in content:
+                    myTasks.write(task+"\n")
+                myTasks.close()
+
                 print("Task has been Done successfully")
+
             else:
                 print("There is no tasks")
             print("[1] End another task...")
             print("[2] Back...")
+
             i = int(input("Enter the number of function: ").strip())
+
             if i == 1:
                 continue
+
             elif i == 2:
                 break
+
             else:
                 print("Enter a valid input")
+
         except:
             print("Enter a Valid task name")
 
 def ViewEndTasks():
-    if len(endTasks) >=1:
-        for endTask in endTasks:
+    if len(open(r"D:\Python Basics\Projects\EndTasks.txt","r",encoding='utf-8').read()) >=1 :
+        endTasks = open(r"D:\Python Basics\Projects\EndTasks.txt","r")
+        Etasks = endTasks.readlines()
+        for endTask in Etasks:
             print(endTask)
+            
     else:
         print("There is no end tasks")
     input("Press enter to get back")
